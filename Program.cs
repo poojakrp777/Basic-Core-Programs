feaba513294407be20f334ea5c76f75a6387ed9b
poojakrp777 <poojakrp777@gmail.com>
2022-09-21 11:28:00 +0530

    Harmonic Number

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace Basic_core_Program
         static void Main(string[] args)
         {
             Console.WriteLine("Please choose any one program from below options");
-            Console.WriteLine("1:Flipcoin \n2:Leap Year \n3.\n4.Palindrome\n5.SumOfDigits\n6.ReverseWord\n7.PrimeNumber\n8.Factorial\n9.Swapping Number\n10.OddorEven");
+            Console.WriteLine("1:Flipcoin \n2:Leap Year \n3.Powerof2\n4.Harmonic Number \n5.\n6.\n7.\n8.\n9.\n10.");
             int choose = Convert.ToInt32(Console.ReadLine());
             switch (choose)
             {
@@ -28,14 +28,15 @@ namespace Basic_core_Program
                     Console.WriteLine("Entered Number to print Power Of two ");
                     Powerof2.powerOf2();
                     break;
-                //case 4:
-                    
-                //    break;
+                case 4:
+                    Console.WriteLine("\t\t The Harmonic vaue of entered number is :");
+                    Harmonic.harmonic();
+                    break;
                 //case 5:
-                    
+
                 //    break;
                 //case 6:
-                    
+
                 //    break;
                 //case 7:
                 //    //PrimeNumber.FindPrimes();
